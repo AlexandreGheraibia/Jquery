347a6b3165277b6bb6bc150d6d1f487a7c404560
gheraibia <gheraibia@hotmail.com>
2018-07-17 20:30:52 +0200

    Update sliderv2.js

--- slider/sliderv2.js
+++ slider/sliderv2.js
@@ -2,33 +2,32 @@ var Slider={
   id:0,
   nbSlides:0,
   widthTotal:0,
-  actualPos:1,
+  actualPos:0,
 
-  init:function (id,nbSlides){
+  init:function (id){
       var self=this;//bonne pratique
       self.id=id;
-      self.nbSlides=nbSlides;
-      self.widthTotal=100*nbSlides;
-      rollover=setInterval(function(){
-          self.nextSlide(1);
-      },2500);
-      $('#monSlidersArrows').on('mouseover',function(){
+      self.widthTotal=100*$('.slide').length;
+      rollover=setInterval(()=>{
+                self.nextSlide(1);
+                },2500);
+      $('#monSlidersArrows').on('mouseover',()=>{
         clearInterval(rollover);
       });
-      $('#monSlidersArrows').on('mouseout',function(){
-          rollover=setInterval(function(){
+      $('#monSlidersArrows').on('mouseout',()=>{
+          rollover=setInterval(()=>{
           self.nextSlide(1);
         },2500);
       });
-      $('#monSlidersArrows div').on('click',function(){
+      $('#monSlidersArrows div').on('click',()=>{
           self.nextSlide(Math.pow(-1,$(this).index()+1));
       });
 
   },
   nextSlide:function(direction){
     var self=this;
-    self.actualPos=(self.nbSlides+self.actualPos+direction)%self.nbSlides;
-    var nextPos=-(self.actualPos)*100+'%';
+    self.actualPos=(self.widthTotal+self.actualPos+direction*100)%self.widthTotal;
+    var nextPos=-self.actualPos+'%';
     $('#monSliderContent').css('left',nextPos);
   }
 }
